b6e1a150d2316a254deca109ef9ad604c445e201
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2021-08-11 17:39:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ Camera = document.getElementById("Camera");
 
 Webcam.attach("Camera");
 
-
 function Capture() {
     Webcam.snap(function (DataUrl) {
         document.getElementById("Result").innerHTML = "<img id='CapturedImage' src='" + DataUrl + "'/>";
@@ -20,7 +19,7 @@ function Capture() {
 
 console.log("ml5 version:", ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/QXwOhu23R/model.json", ModelLoaded);
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/FJtJTSZy8/model.json", ModelLoaded);
 
 function ModelLoaded() {
     console.log("Model Loaded!");
@@ -31,4 +30,29 @@ function Speak() {
     SpeakData = "The prediction is " + Prediction;
     var UtterThis = new SpeechSynthesisUtterance(SpeakData);
     Synth.speak(UtterThis);
+}
+
+function Check() {
+    Img = document.getElementById("CapturedImage");
+    Classifier.classify(Img, GetResult);
+}
+
+function GetResult(Error, Results) {
+    if (Error) {
+        console.error(Error);
+    } else {
+        console.log(Results);
+        document.getElementById("GestureName").innerHTML = Results[0].label;
+        Prediction = Results[0].label;
+        Speak();
+        if (Results[0].label == "Amazing") {
+            document.getElementById("Gesture").innerHTML = "&#128076;";
+        }
+        if (Results[0].label == "Best") {
+            document.getElementById("Gesture").innerHTML = "&#128077;";
+        }
+        if (Results[0].label == "Victory") {
+            document.getElementById("Gesture").innerHTML = "&#9996;";
+        }
+    }
 }
